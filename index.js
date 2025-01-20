57adbf09536e77f2ddd9e955ebb8d2ddd7dd3ee5
anshsaini <anshsainihtml@gmail.com>
2025-01-20 05:40:34 -0800

    portfolio

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 // server.js (backend)
 const express = require("express");
-
-
+const path = require("path")
 const app = express();
 const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.use(express.json());
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index.ejs");
-} )
+})
 
+app.get('/home', (req, res) => {
+  res.render('home.ejs')
+})
 // Route to handle image generation
 app.post("/name", async (req, res) => {
- let a = 'h1'
 
-  res.writeHead(200, {'content-type': 'text/html'})
-  
-  console.log(req.body)
-
-  const { name } = req.body;
-  console.log(name);
-  const h1 = `<h1>hello  ${name}</h1>`
-  res.end(h1);
+res.end(h1);
 
 })
 
